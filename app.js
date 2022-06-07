5fd6af57791423df33b467303329cd0fd31bd86a
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 20:01:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// arrow functions
+
+let viajando = (destino) => `Viajando a la ciudad de: ${destino}`
+
+let viaje
+viaje = viajando('París')
+viaje = viajando('Londres')
+viaje = viajando('Barcelona')
+console.log(viaje)
